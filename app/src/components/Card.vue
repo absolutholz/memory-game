e71aebef7422b72d33d72c181597648419278680
swood <stephen.wood@hoffmann-group.com>
2020-11-16 17:00:06 +0100

    Add card flip animation

--- app/src/components/Card.vue
+++ app/src/components/Card.vue
@@ -4,14 +4,16 @@
 		:class="`card${ isShowing ? ' card--showing' : '' }${ isFound ? ' card--found' : '' }`"
 		:disabled="isShowing || isFound"
 	>
-		<div class="card__face" hidden>
-			<slot />
-		</div>
-		<div class="card__back">
-			<span v-if="$route.query.debug" style="position: relative; z-index: 10;">{{ name }}</span>
-			<svg class="card__image card__image--svg" viewBox="0 0 24 24">
-				<path fill="currentColor" d="M11,13.5V21.5H3V13.5H11M12,2L17.5,11H6.5L12,2M17.5,13C20,13 22,15 22,17.5C22,20 20,22 17.5,22C15,22 13,20 13,17.5C13,15 15,13 17.5,13Z" />
-			</svg>
+		<div class="card__content">
+			<div class="card__back">
+				<span v-if="$route.query.debug" style="position: relative; z-index: 10;">{{ name }}</span>
+				<svg class="card__image card__image--svg" viewBox="0 0 24 24">
+					<path fill="currentColor" d="M11,13.5V21.5H3V13.5H11M12,2L17.5,11H6.5L12,2M17.5,13C20,13 22,15 22,17.5C22,20 20,22 17.5,22C15,22 13,20 13,17.5C13,15 15,13 17.5,13Z" />
+				</svg>
+			</div>
+			<div class="card__face">
+				<slot />
+			</div>
 		</div>
 	</button>
 </template>
@@ -56,46 +58,56 @@ export default {
 </script>
 
 <style lang="scss">
-$background: white;
+// https://codepen.io/edeesims/pen/iGDzk?editors=0100
+// https://codepen.io/cojdev/pen/EQZVRN?editors=0100
+// https://codepen.io/ettrics/pen/zxMPWj?editors=0100
 
 .card {
-	background: $background;
-	box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
-	border-radius: 4px;
 	display: block;
 	padding-bottom: 100%;
+	perspective: 500px;
 	position: relative;
-	transition: box-shadow 250ms, transform 250ms, opacity 250ms 2s;
+	transition: opacity 250ms 2s;
 	width: 100%;
+
+	&__content {
+		border-radius: 4px;
+		box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
+		height: 100%;
+		position: absolute;
+		transform-style: preserve-3d;
+		transition: transform 1s;
+		width: 100%;
+
+		.card--showing & {
+			box-shadow: 0 5px 5px -3px rgba(0, 0, 0, 0.2), 0 8px 10px 1px rgba(0, 0, 0, 0.14), 0 3px 14px 2px rgba(0, 0, 0, 0.12);
+			transform: rotateY(180deg) scale(1.125);
+			transition: transform 0.5s;
+		}
+	}
 
 	&__back,
 	&__face {
 		align-items: center;
-		border: 4px solid dodgerblue;
+		background: white;
 		border-radius: inherit;
 		display: flex;
 		height: 100%;
 		justify-content: center;
 		left: 0;
 		position: absolute;
+		backface-visibility: hidden;
 		top: 0;
+		transform-style: preserve-3d;
 		width: 100%;
 	}
 
 	&__back {
 		color: dodgerblue;
-
-		.card--showing & {
-			display: none;
-		}
 	}
 
 	&__face {
-		display: none;
-
-		.card--showing & {
-			display: block;
-		}
+		transform: rotateY( 180deg );
 	}
 
 	&__image {
@@ -105,6 +117,7 @@ $background: white;
 		position: absolute;
 		top: 10%;
 		width: 80%;
+		z-index: 0;
 
 		&--svg {
 
@@ -113,11 +126,6 @@ $background: white;
 
 	&--found {
 		opacity: 0;
-	}
-
-	&--showing {
-		box-shadow: 0 5px 5px -3px rgba(0, 0, 0, 0.2), 0 8px 10px 1px rgba(0, 0, 0, 0.14), 0 3px 14px 2px rgba(0, 0, 0, 0.12);
-		transform: scale(1.125);
 	}
 }
 </style>
