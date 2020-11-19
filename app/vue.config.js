54419569a5cd4a105c8283b03fcd39e112bee7f7
swood <stephen.wood@hoffmann-group.com>
2020-11-19 14:23:07 +0100

    Update title for document and manifest

---
+++ app/vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	pwa: {
+		manifestOptions: {
+			name: 'Memory',
+			short_name: 'Memory @ absolutholz.de',
+			description: 'A simple card memory game',
+			display: 'fullscreen',
+			background_color: '#fffff8',
+		},
+	},
+};
