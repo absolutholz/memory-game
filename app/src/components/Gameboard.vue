860d6ae63b4717fad1aefdfa307a67caab4d9dfd
absolutholz <absolutholz@gmail.com>
2020-11-21 23:00:27 +0100

    Remove gameboard borders

--- app/src/components/Gameboard.vue
+++ app/src/components/Gameboard.vue
@@ -19,12 +19,12 @@ export default {
 .gameboard {
 	--space: 0.5rem;
 
-	background: #ccc;
+	// background: #ccc;
 	bottom: 0;
 	display: flex;
 	flex-direction: column;
 	left: 0;
-	padding: var(--spacing-mini);
+	// padding: var(--spacing-mini);
 	position: fixed;
 	right: 0;
 	top: 0;
@@ -35,10 +35,10 @@ export default {
 
 	&__playing-field {
 		align-items: center;
-		background: #fffff8;
-		border: 2px solid #aaa;
-		border-radius: 1rem;
-		box-shadow: inset 10px 10px 20px -10px #333;
+		// background: #fffff8;
+		// border: 2px solid #aaa;
+		// border-radius: 1rem;
+		// box-shadow: inset 10px 10px 20px -10px #333;
 		display: flex;
 		flex: 1 1 auto;
 		justify-content: center;
@@ -47,16 +47,20 @@ export default {
 	}
 
 	&__scoreboard {
-		background: #fffff8;
-		border: 2px solid #aaa;
-		border-radius: 1rem;
-		box-shadow: inset 10px 10px 20px -10px #333;
+		align-items: center;
+		// background: #fffff8;
+		// border: 2px solid #aaa;
+		// border-radius: 1rem;
+		// box-shadow: inset 10px 10px 20px -10px #333;
+		display: flex;
 		flex: 0 0 auto;
+		justify-content: center;
 		margin-top: var(--spacing-mini);
 		overflow: hidden;
 		padding: 1rem;
 
 		@media (orientation: landscape) {
+			flex-direction: column;
 			margin-left: var(--spacing-mini);
 			margin-top: 0;
 		}
