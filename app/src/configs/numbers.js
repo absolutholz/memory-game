b5c5a72d8227e53deb4975fb9fb8a42729c86fe9
absolutholz <absolutholz@gmail.com>
2020-12-03 06:36:41 +0100

    Update number config value to be strings

--- app/src/configs/numbers.js
+++ app/src/configs/numbers.js
@@ -12,91 +12,91 @@ const config = {
 
 	images: [
 		{
-			src: 0,
+			src: '0',
 		},
 		{
-			src: 1,
+			src: '1',
 		},
 		{
-			src: 2,
+			src: '2',
 		},
 		{
-			src: 3,
+			src: '3',
 		},
 		{
-			src: 4,
+			src: '4',
 		},
 		{
-			src: 5,
+			src: '5',
 		},
 		{
-			src: 6,
+			src: '6',
 		},
 		{
-			src: 7,
+			src: '7',
 		},
 		{
-			src: 8,
+			src: '8',
 		},
 		{
-			src: 9,
+			src: '9',
 		},
 		{
-			src: 10,
+			src: '10',
 		},
 		{
-			src: 11,
+			src: '11',
 		},
 		{
-			src: 12,
+			src: '12',
 		},
 		{
-			src: 13,
+			src: '13',
 		},
 		{
-			src: 14,
+			src: '14',
 		},
 		{
-			src: 15,
+			src: '15',
 		},
 		{
-			src: 16,
+			src: '16',
 		},
 		{
-			src: 17,
+			src: '17',
 		},
 		{
-			src: 18,
+			src: '18',
 		},
 		{
-			src: 19,
+			src: '19',
 		},
 		{
-			src: 20,
+			src: '20',
 		},
 		{
-			src: 21,
+			src: '21',
 		},
 		{
-			src: 22,
+			src: '22',
 		},
 		{
-			src: 23,
+			src: '23',
 		},
 		{
-			src: 24,
+			src: '24',
 		},
 		{
-			src: 25,
+			src: '25',
 		},
 		{
-			src: 26,
+			src: '26',
 		},
 		{
-			src: 27,
+			src: '27',
 		},
 		{
-			src: 28,
+			src: '28',
 		},
 	],
 };
