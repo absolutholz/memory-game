904beef7c93df8528015492ac6e22128aaa05e92
absolutholz <absolutholz@gmail.com>
2020-12-08 16:51:26 +0100

    Add player creator list

---
+++ app/src/components/PlayerCreatorList/PlayerCreatorList.stories.js
@@ -0,0 +1,29 @@
+import PlayerCreatorList from '.';
+import { Player } from './../PlayerCreator';
+
+export default {
+	title: 'Player Creator List',
+};
+
+export const WithoutPlayers = () => ({
+	components: { PlayerCreatorList },
+	template: `
+			<player-creator-list />
+	`,
+});
+
+export const WithPlayers = () => ({
+	components: { PlayerCreatorList },
+	template: `
+			<player-creator-list :players="players" />
+	`,
+	props: {
+		players: {
+			default: [
+				new Player('1', 'Vroni'),
+				new Player('2', 'Nicky'),
+				new Player('3', 'Daddy'),
+			],
+		},
+	},
+});
---
+++ app/src/components/PlayerCreatorList/PlayerCreatorList.vue
@@ -0,0 +1,53 @@
+<template>
+	<ol class="player-grid">
+		<li
+			v-for="(player, index) in players" :key="index"
+		>
+			<player-creator :player="player" />
+		</li>
+	</ol>
+</template>
+
+<script>
+import PlayerCreator from './../PlayerCreator';
+
+import { Player } from './../PlayerCreator';
+
+export default {
+	name: 'PlayerCreatorList',
+
+	components: {
+		PlayerCreator,
+	},
+
+	props: {
+		players: {
+			default: () => {
+				return [
+					new Player('1'),
+					new Player('2'),
+				];
+			},
+			required: false,
+			type: Array,
+		},
+	},
+
+};
+</script>
+
+<style lang="scss">
+.player-grid {
+	display: grid;
+	grid-auto-rows: 1fr;
+	grid-gap: var(--spacing-base) var(--spacing-base);
+	grid-template-columns: repeat(auto-fill, minmax(clamp(50% #{"-"} var(--spacing-base), 25ch, 100%), 1fr));
+	list-style: none;
+	margin: var(--spacing-base) 0;
+	padding-left: 0;
+
+	> * {
+		margin: 0;
+	}
+}
+</style>
