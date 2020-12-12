4d345ad08042f6636e77f4c015cfa2a1d05f6afc
absolutholz <absolutholz@gmail.com>
2020-12-12 21:23:32 +0100

    Update card module with colors

--- app/src/js/Card.js
+++ app/src/js/Card.js
@@ -5,4 +5,35 @@ export default function Card (id, name) {
 		src: '',
 		type: 'text',
 	};
+	this.color = 'inherit';
 }
+
+export const colors = [
+	'#b71c1c',
+	'#880e4f',
+	'#4a148c',
+	'#311b92',
+	'#1a237e',
+	'#0d47a1',
+	'#01579b',
+	'#006064',
+	'#004d40',
+	'#1b5e20',
+	'#33691e',
+	'#827717',
+	'#f57f17',
+	'#ff6f00',
+	'#e65100',
+	'#bf360c',
+	'#3e2723',
+	'#212121',
+	'#263238',
+	'firebrick',
+	'chocolate',
+	'dodgerblue',
+	'rebeccapurple',
+	'forestgreen',
+	'teal',
+	'tomato',
+	'limegreen',
+];
--- app/src/js/Player.js
+++ app/src/js/Player.js
@@ -1,4 +1,5 @@
 export default function Player (id, name = 'Player') {
 	this.id = id;
 	this.name = name;
+	this.cards = [];
 }
