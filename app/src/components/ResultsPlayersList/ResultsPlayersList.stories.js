8c76e5258361f65090f66e8d2060d61bd6e90df0
absolutholz <absolutholz@gmail.com>
2020-12-15 15:04:36 +0100

    Add results players list component

---
+++ app/src/components/ResultsPlayersList/ResultsPlayersList.stories.js
@@ -0,0 +1,78 @@
+import ResultsPlayersList from '.';
+import ResultsPlayer from './../ResultsPlayer';
+
+import Card from './../../js/Card';
+import Player from './../../js/Player';
+
+const colors = [
+	'firebrick',
+	'forestgreen',
+	'rebeccapurple',
+	'dodgerblue',
+	'coral',
+]
+
+const cards = [];
+for (let i = 0; i < 5; i += 1) {
+	const card1 = new Card(`${i}-a`, `${i}`);
+	card1.faceImage.src = `${i}`;
+	card1.faceImage.type = 'text';
+	card1.color = colors[i];
+
+	const card2 = new Card(`${i}-b`, `${i}`);
+	card2.faceImage.src = `${i}`;
+	card2.faceImage.type = 'text';
+	card1.color = colors[i];
+
+	cards.push(card1);
+	cards.push(card2);
+}
+
+const player1 = new Player('1', 'Player 1');
+player1.cards = cards;
+
+const player2 = new Player('2', 'Player 2');
+player2.cards = cards;
+
+const player3 = new Player('3', 'Player 3');
+player3.cards = cards;
+
+const player4 = new Player('4', 'Player 4');
+player4.cards = cards;
+
+export default {
+	title: 'Results/Players List',
+	component: ResultsPlayersList,
+};
+
+const Template = (args, { argTypes }) => ({
+	components: { ResultsPlayersList, ResultsPlayer },
+	props: Object.keys(argTypes),
+	template:  `
+		<results-players-list v-bind="$props">
+			<template v-slot:default="{ player }">
+				<results-player :player="player" />
+			</template>
+		</results-players-list>
+	`,
+});
+
+export const SinglePlayer = Template.bind({});
+SinglePlayer.args = {
+	players: [ player1 ],
+};
+
+export const TwoPlayer = Template.bind({});
+TwoPlayer.args = {
+	players: [ player1, player2 ],
+};
+
+export const ThreePlayer = Template.bind({});
+ThreePlayer.args = {
+	players: [ player1,player2, player3 ],
+};
+
+export const FourPlayer = Template.bind({});
+FourPlayer.args = {
+	players: [ player1, player2, player3, player4 ],
+};
---
+++ app/src/components/ResultsPlayersList/ResultsPlayersList.vue
@@ -0,0 +1,36 @@
+<template>
+	<ol class="results-players-list">
+		<li v-for="(player, index) in players" :key="`list-item-${index}`">
+			<slot v-bind:player="player" />
+		</li>
+	</ol>
+</template>
+
+<script>
+export default {
+	name: 'ResultsPlayersList',
+
+	props: {
+		players: {
+			required: true,
+			type: Array,
+		},
+	},
+};
+</script>
+
+<style lang="scss">
+.results-players-list {
+	align-items: stretch;
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	list-style: none;
+	margin: calc(var(--spacing-base) * -1);
+	padding-left: 0;
+
+	> li {
+		margin: var(--spacing-base);
+	}
+}
+</style>
