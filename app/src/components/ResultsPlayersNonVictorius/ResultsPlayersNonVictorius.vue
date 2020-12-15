3c43ff35437cc478e58955bc3759b9a5f87cc448
absolutholz <absolutholz@gmail.com>
2020-12-15 14:34:58 +0100

    Add non-victorius players component

---
+++ app/src/components/ResultsPlayersNonVictorius/ResultsPlayersNonVictorius.vue
@@ -0,0 +1,49 @@
+<template>
+	<section class="results-non-victorius">
+		<h2 class="results-victorius__hdln">Better luck next time</h2>
+
+		<div v-if="players.length === 1">
+			<results-player :player="players[0]" />
+		</div>
+
+		<div v-else>
+			<results-players-list :players="players">
+				<template v-slot:default="{ player }">
+					<results-player :player="player" />
+				</template>
+			</results-players-list>
+		</div>
+
+	</section>
+</template>
+
+<script>
+import ResultsPlayersList from './../ResultsPlayersList';
+import ResultsPlayer from './../ResultsPlayer';
+
+export default {
+	name: 'ResultsPlayersNonVictorius',
+
+	components: {
+		ResultsPlayersList,
+		ResultsPlayer,
+	},
+
+	props: {
+		players: {
+			required: true,
+			type: Array,
+		},
+	},
+};
+</script>
+
+<style lang="scss">
+.results-non-victorius {
+	&__hdln {
+		font-size: 4rem;
+		margin: 0 0 var(--spacing-mega);
+		text-align: center;
+	}
+}
+</style>
