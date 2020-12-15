780bc5987b97573f4943d6cb652f827b133cb6ac
absolutholz <absolutholz@gmail.com>
2020-12-15 14:34:35 +0100

    Add victorius player component

---
+++ app/src/components/ResultsPlayersVictorius/ResultsPlayersVictorius.stories.js
@@ -0,0 +1,73 @@
+import ResultsPlayersVictorius from '.';
+
+import Card from './../../js/Card';
+import Player from './../../js/Player';
+
+const colors = [
+	'firebrick',
+	'forestgreen',
+	'rebeccapurple',
+	'dodgerblue',
+	'coral',
+]
+
+const cards = [];
+for (let i = 0; i < 5; i += 1) {
+	const card1 = new Card(`${i}-a`, `${i}`);
+	card1.faceImage.src = `${i}`;
+	card1.faceImage.type = 'text';
+	card1.color = colors[i];
+
+	const card2 = new Card(`${i}-b`, `${i}`);
+	card2.faceImage.src = `${i}`;
+	card2.faceImage.type = 'text';
+	card1.color = colors[i];
+
+	cards.push(card1);
+	cards.push(card2);
+}
+
+const player1 = new Player('1', 'Player 1');
+player1.cards = cards;
+
+const player2 = new Player('2', 'Player 2');
+player2.cards = cards;
+
+const player3 = new Player('3', 'Player 3');
+player3.cards = cards;
+
+const player4 = new Player('4', 'Player 4');
+player4.cards = cards;
+
+export default {
+	title: 'Results/Players Victorius',
+	component: ResultsPlayersVictorius,
+};
+
+const Template = (args, { argTypes }) => ({
+	components: { ResultsPlayersVictorius },
+	props: Object.keys(argTypes),
+	template:  `
+		<results-players-victorius v-bind="$props" />
+	`,
+});
+
+export const SinglePlayer = Template.bind({});
+SinglePlayer.args = {
+	players: [ player1 ],
+};
+
+export const TwoPlayer = Template.bind({});
+TwoPlayer.args = {
+	players: [ player1, player2 ],
+};
+
+export const ThreePlayer = Template.bind({});
+ThreePlayer.args = {
+	players: [ player1,player2, player3 ],
+};
+
+export const FourPlayer = Template.bind({});
+FourPlayer.args = {
+	players: [ player1, player2, player3, player4 ],
+};
---
+++ app/src/components/ResultsPlayersVictorius/ResultsPlayersVictorius.vue
@@ -0,0 +1,50 @@
+<template>
+	<section class="results-victorius">
+
+		<div v-if="players.length === 1">
+			<h2 class="results-victorius__hdln">Victory!</h2>
+			<results-player-victorius :player="players[0]" />
+		</div>
+
+		<div v-else>
+			<h2 class="results-victorius__hdln">It's a draw!</h2>
+			<results-players-list :players="players">
+				<template v-slot:default="{ player }">
+					<results-player-victorius :player="player" />
+				</template>
+			</results-players-list>
+		</div>
+
+	</section>
+</template>
+
+<script>
+import ResultsPlayersList from './../ResultsPlayersList';
+import ResultsPlayerVictorius from './../ResultsPlayerVictorius';
+
+export default {
+	name: 'ResultsPlayersVictorius',
+
+	components: {
+		ResultsPlayersList,
+		ResultsPlayerVictorius,
+	},
+
+	props: {
+		players: {
+			required: true,
+			type: Array,
+		},
+	},
+};
+</script>
+
+<style lang="scss">
+.results-victorius {
+	&__hdln {
+		font-size: 4rem;
+		margin: 0 0 var(--spacing-mega);
+		text-align: center;
+	}
+}
+</style>
