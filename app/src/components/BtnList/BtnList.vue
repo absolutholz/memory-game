f84c145ac2c7bc7036421c8ce58d5be487e44da3
absolutholz <absolutholz@gmail.com>
2020-12-16 13:02:44 +0100

    Add btn list component

---
+++ app/src/components/BtnList/BtnList.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="btn-list">
+		<slot />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'BtnList',
+};
+</script>
+
+<style lang="scss">
+.btn-list {
+	margin: calc(var(--spacing-mini) #{"*"} -1) calc(var(--spacing-base) #{"*"} -1);
+
+	> * {
+		margin: var(--spacing-mini) var(--spacing-base);
+	}
+}
+</style>
