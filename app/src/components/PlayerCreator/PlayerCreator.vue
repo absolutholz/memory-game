8ac7c94921b2526a4db23fa2fb7de4c4b843e669
absolutholz <absolutholz@gmail.com>
2020-12-16 15:33:12 +0100

    Add avatar to player creator

--- app/src/components/PlayerCreator/PlayerCreator.vue
+++ app/src/components/PlayerCreator/PlayerCreator.vue
@@ -1,26 +1,47 @@
 <template>
-	<input-group
-	>
-		<template #label>Player name</template>
-		<input
-			v-model="proxyName"
-			class="input"
-			maxlength="20"
-			minlength="3"
-			required
-			type="text"
-		/>
-		<btn
-			@click="deletePlayer"
-			class="compound-input__suffix"
-			type="button"
-		><svg-player-delete aria-hidden="true" aria-label="remove player" class="icon" title="remove player" /></btn>
-	</input-group>
+	<card class="player-creator">
+		<div class="player-creator__image">
+			<avatar
+				@click.native="changeAvatar"
+				class="player-creator__avatar"
+				:text="proxyName"
+			/>
+		</div>
+
+		<div class="player-creator__main">
+			<input-group>
+				<template #label>Player name</template>
+				<input-wrapper>
+					<input
+						v-model="proxyName"
+						class="input"
+						maxlength="20"
+						minlength="3"
+						required
+						type="text"
+					/>
+				</input-wrapper>
+			</input-group>
+		</div>
+
+		<div class="player-creator__actions">
+			<btn
+				@click="deletePlayer"
+				class="btn--subtle btn--small"
+				type="button"
+			>
+				<svg-player-delete aria-hidden="true" aria-label="remove player" class="icon" title="remove player" /> Remove
+			</btn>
+		</div>
+	</card>
 </template>
 
 <script>
+import Avatar from './../Avatar';
 import Btn from './../Btn';
+import Card from './../Card';
 import InputGroup from './../InputGroup';
+import InputWrapper from './../InputWrapper';
 
 import SvgPlayerDelete from '@mdi/svg/svg/account-minus.svg';
 
@@ -30,8 +51,11 @@ export default {
 	name: 'PlayerConfig',
 
 	components: {
+		Avatar,
 		Btn,
+		Card,
 		InputGroup,
+		InputWrapper,
 
 		SvgPlayerDelete,
 	},
@@ -57,6 +81,40 @@ export default {
 		deletePlayer () {
 			this.$emit('delete', this.player);
 		},
+
+		changeAvatar () {
+
+		},
 	},
 };
 </script>
+
+<style lang="scss">
+.player-creator {
+	display: flex;
+	flex-wrap: wrap;
+	padding: var(--spacing-base);
+
+	&__image {
+		margin-right: var(--spacing-base);
+	}
+
+	&__main {
+		flex: 1 1 auto;
+	}
+
+	&__avatar {
+		width: 4rem;
+	}
+
+	&__actions {
+		text-align: right;
+		width: 100%;
+	}
+
+	.input-group {
+		margin: 0;
+		width: 100%;
+	}
+}
+</style>
