328ec481bcb9630e5bbf5a96f62a1b3bc36e1cd4
absolutholz <absolutholz@gmail.com>
2020-12-19 15:38:50 +0100

    update gameboard player story structure

--- app/src/components/GameboardPlayers/GameboardPlayers.stories.js
+++ app/src/components/GameboardPlayers/GameboardPlayers.stories.js
@@ -1,14 +1,14 @@
 import GameboardPlayers from '.';
 import Player from './../../js/Player';
 
+const player1 = new Player('1', 'Player 1');
+player1.cards = new Array(10);
+const player2 = new Player('2', 'Player 2');
+
 export default {
 	title: 'Gameboard/Gameboard Players',
 	component: GameboardPlayers,
 };
-
-const player1 = new Player('1', 'Player 1');
-player1.cards = new Array(10);
-const player2 = new Player('2', 'Player 2');
 
 const Template = (args, { argTypes }) => ({
 	components: { GameboardPlayers },
